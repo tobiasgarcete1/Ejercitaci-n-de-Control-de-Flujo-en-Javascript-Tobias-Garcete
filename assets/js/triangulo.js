09fab4c40f6afa8ab6684601180c1ea457fce048
Your Name <you@example.com>
2024-05-13 11:15:45 -0300

    Se realizo el trabajo de triangulo.js

---
+++ assets/js/triangulo.js
@@ -0,0 +1,12 @@
+
+let altura = 5;
+
+for (let i = 1; i <= altura; i++) {
+    let linea = ''; 
+    
+    
+    for (let j = 1; j <= i; j++) {
+        linea += '#';
+    }
+    console.log(linea);
+}
