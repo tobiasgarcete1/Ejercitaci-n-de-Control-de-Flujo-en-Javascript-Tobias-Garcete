fc5990449fe6f5f431462055d8af1b463cf302c0
Your Name <you@example.com>
2024-05-13 11:09:14 -0300

    Se realizo el trabajo de parImpar.js

---
+++ assets/js/parImpar.js
@@ -0,0 +1,7 @@
+for (let numero = 0; numero <= 10; numero++) {
+    if (numero % 2 === 0) {
+        console.log(numero + " es par");
+    } else {
+        console.log(numero + " es impar");
+    }
+}
